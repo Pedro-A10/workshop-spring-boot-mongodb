de86443e599836bbd1ffa884fd24b749ae2a79f6
Pedro-A10 <pedroaugusto732@gmail.com>
2025-04-20 16:56:41 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/pedroa10/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroa10.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
