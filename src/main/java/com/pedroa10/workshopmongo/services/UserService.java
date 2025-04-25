31ebfbe57c9a5ead30144749d6492ed3f45cf57c
Pedro-A10 <pedroaugusto732@gmail.com>
2025-04-24 21:10:09 -0300

    Obtendo um usuário por id

--- src/main/java/com/pedroa10/workshopmongo/services/UserService.java
+++ src/main/java/com/pedroa10/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.pedroa10.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedroa10.workshopmongo.domain.User;
 import com.pedroa10.workshopmongo.repository.UserRepository;
+import com.pedroa10.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/com/pedroa10/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.pedroa10.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
