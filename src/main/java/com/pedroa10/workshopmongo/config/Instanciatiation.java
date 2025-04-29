f3d1da3775c0be3fcb441415f36c12c267849bee
Pedro-A10 <pedroaugusto732@gmail.com>
2025-04-28 21:00:41 -0300

    Referenciando os posts do usuário

--- src/main/java/com/pedroa10/workshopmongo/config/Instanciatiation.java
+++ src/main/java/com/pedroa10/workshopmongo/config/Instanciatiation.java
@@ -42,5 +42,8 @@ public class Instanciatiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("28/04/2025"), "TFT BEST GAME OF THE YEAR", "TFT É BAUM D++++", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
